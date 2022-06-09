aec38f6fdd77595461a49b03328264169b2cea49
Brad0788 <89504455+Brad0788@users.noreply.github.com>
2022-06-08 19:58:31 -0600

    Add files via upload

--- sorted_array.asm
+++ sorted_array.asm
@@ -1,5 +1,6 @@
 .data
 array: .space 20
+array2: .space 20
 msg: .asciiz "\n Numero: "
 .text
 .globl main
@@ -29,9 +30,54 @@ while:
     addi $t0, $t0, 1 # i++
     blt $t0, 5, while
 
+    li $t0, 0
+    la $t1, array
+    la $t8, array2
+
 while_pares:
+    lw $t2, 0($t1)
     
+    addi $t1, $t1, 4
+    andi $t9, $t2, 1
+    addi $t0, $t0, 1
+    beq $t9, $zero, insert_par
+    blt $t0, 5, while_pares
+    li $t0, 0
+    la $t1, array
+    j while_impares
+
+insert_par:
+    sw $t2, 0($t8)
+    addi $t8, $t8, 4
+    j while_pares 
 
 while_impares:
-
+    lw $t2, 0($t1)
     
+    addi $t1, $t1, 4
+    andi $t9, $t2, 1
+    addi $t0, $t0, 1
+    bne $t9, $zero, insert_impar
+    blt $t0, 5, while_impares
+    li $t0, 0 #i = 0
+    la $t1, array2 #pos = 0
+    j print_array
+    
+insert_impar:
+    sw $t2, 0($t8)
+    addi $t8, $t8, 4
+    j while_impares
+
+print_array:
+
+
+    lw $t2, 0($t1)
+    addi $t1, $t1, 4
+    addi $t0, $t0, 1
+    move $a0, $t2
+    li $v0, 1
+    syscall
+    blt $t0, 5, print_array
+
+    li $v0, 10
+    syscall
